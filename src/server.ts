9415fbe25fc6e5b600f375451b0910ecca4de306
smelel <samuelbettin@gmail.com>
2025-05-08 23:07:16 -0300

    Atualiza migrações e schema para PostgreSQL no Render

--- src/server.ts
+++ src/server.ts
@@ -15,4 +15,7 @@ app.use('/missoes', missoes);
 app.use('/modulos', modulos);
 app.use('/participacoes', participacoes); 
 
-app.listen(3001, () => console.log('API rodando em http://localhost:3001'));
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`API rodando em http://localhost:${port}`);
+});
